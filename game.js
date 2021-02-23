847a1d2987a98f9a24d94b7a734ba213caea4f44
KarmaConfigs <karmaconfigs@gmail.com>
2021-02-23 20:04:55 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 (() => {
     "use strict";
     var t, e, r = function(t) {
-            t.style.textColor = "rgb(230, 230, 230)";
+            t.style.textColor = "rgb(184, 184, 184)";
         },
         n = function(t) {
             var e = function() {
