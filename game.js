acd4877da760796ada8f565ca4ef722f1301c2c0
KarmaConfigs <karmaconfigs@gmail.com>
2021-02-23 19:33:47 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 (() => {
     "use strict";
     var t, e, r = function(t) {
-            t.style.opacity = "20%";
+            t.style.opacity = "90%";
             t.innerText = t.innerText + ".";
         },
         n = function(t) {
